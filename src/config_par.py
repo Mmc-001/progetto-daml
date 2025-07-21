d9fe69bf7aafbe0f77ac4e005ae2d60a9b3fe4dc
Mmc-001 <matteomc.01@gmail.com>
2025-07-21 16:39:13 +0200

    Update model training configurations and add initial CNN architecture

--- src/config_par.py
+++ src/config_par.py
@@ -1,34 +1,41 @@
 # useful constants for each architecture
 # to be used in the model_training.ipynb notebook
 IMG_SIZE            = (128, 128) # Image size for resizing
-BATCH_SIZE          = [64,
+BATCH_SIZE          = [0,
+                       64,
                        64,
                        64,
                        64,]
-N_EPOCHS            = [50,
+N_EPOCHS            = [30,
+                       50,
                        30,
                        30,
                        30,]
-DROP_RATE           = [0.3,
+DROP_RATE           = [0.2,
+                       0.3,
                        0.3,
                        0.3,
                        0.3]
 L2_REGULARIZATION   = [0.005,
                        0.005,
                        0.005,
+                       0.005,
                        0.005]
-STARTING_LR         = [0.0001,
+STARTING_LR         = [0.0002,
+                       0.0001,
                        0.0002,
                        0.0002,
                        0.0002]
 EARLY_PATIENCE      = [4,
                        4,
                        4,
+                       4,
                        4]
-REDUCE_LR_PATIENCE = [3,
+REDUCE_LR_PATIENCE = [2,
+                      3,
                       2,
                       2,
                       2]
-TRAIN               = False # Change to False to only visualize an already trained model's history
+TRAIN               = True # Change to False to only visualize an already trained model's history
 
-ARCH_CHOICE        = 1 # Choose the architecture version
+ARCH_CHOICE        = 0 # Choose the architecture version
