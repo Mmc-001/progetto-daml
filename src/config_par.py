e61ecdeae81420d42a629cfd112544ed33980299
Mmc-001 <matteomc.01@gmail.com>
2025-07-21 12:42:21 +0200

    Refactor model training configuration to use a separate constants file for improved organization and maintainability

---
+++ src/config_par.py
@@ -0,0 +1,34 @@
+# useful constants for each architecture
+# to be used in the model_training.ipynb notebook
+IMG_SIZE            = (128, 128) # Image size for resizing
+BATCH_SIZE          = [64,
+                       64,
+                       64,
+                       64,]
+N_EPOCHS            = [50,
+                       30,
+                       30,
+                       30,]
+DROP_RATE           = [0.3,
+                       0.3,
+                       0.3,
+                       0.3]
+L2_REGULARIZATION   = [0.005,
+                       0.005,
+                       0.005,
+                       0.005]
+STARTING_LR         = [0.0001,
+                       0.0002,
+                       0.0002,
+                       0.0002]
+EARLY_PATIENCE      = [4,
+                       4,
+                       4,
+                       4]
+REDUCE_LR_PATIENCE = [3,
+                      2,
+                      2,
+                      2]
+TRAIN               = False # Change to False to only visualize an already trained model's history
+
+ARCH_CHOICE        = 1 # Choose the architecture version
